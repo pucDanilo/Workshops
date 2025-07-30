b92b04fd536517dd40fa354acda9ba5763537e7f
Danilo Pereira da Silva <danilo@pucminas.br>
2025-07-30 15:05:06 -0300

    add: fallback de banco

--- Workshop8/Infraestructure/LogService.cs
+++ Workshop8/Infraestructure/LogService.cs
@@ -1,12 +1,28 @@
+using Microsoft.Extensions.Configuration;
 using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+using Workshop8.Models;
 
 namespace Workshop8.Infrastructure
 {
     public class LogService
     {
+        public LogService(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         public async Task WriteProviderLogAsync(string message)
         {
-            // TODO: implementar gravação de logs em LogsLocal ou remoto
+            var useRemote = Configuration.GetValue<bool>("Database:UseRemote");
+            var cstr = useRemote
+                ? Configuration.GetConnectionString("Remote") ?? Configuration["Database:Remote"]
+                : Configuration.GetConnectionString("Local") ?? Configuration["Database:Local"];
+
+            var providerLabel = useRemote ? "REMOTO" : "LOCAL";
+
         }
     }
 }
